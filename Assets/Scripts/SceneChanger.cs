5d9655e6fe507a27b9c8b9d65878e9e391ab8c64
totopizza <gren6158@yahoo.co.jp>
2016-10-06 16:01:11 +0900

    Scriptsファイル追加、SceneChangerスクリプト追加

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string NextSceneName;
+
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene(NextSceneName);
+    }
+}
