701fe394d9d5ee757ca5f1b370650693c8dc69b9
ryouchida <soccer.ry05@gmail.com>
2016-10-19 16:52:09 +0900

    ゲームメインのUIを配置

---
+++ Assets/uchida/Script/CageManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CageManager : MonoBehaviour
+{
+    enum Sexuality
+    {
+        MALE,
+        FEMALE,
+    }
+
+    struct AnimalStatus
+    {
+        // ID
+        int id;
+        // 名前
+        string name;
+        // 購入金額
+        int purchasePrice;
+        // 一日にかかる食費
+        int foodCost;
+        // 愛情度
+        int loveDegree;
+        // 次のレベルまでの値
+        int nextLevel;
+        // 呼び込める客の数
+        int attractNum;
+        // 性別
+        Sexuality sexuality;
+        // レア度
+        int rarity;
+    }
+    private AnimalStatus Status { get; set; }
+
+    bool IsActive { get; set; }
+
+    void Start()
+    {
+        StartCoroutine(UpdateCage());
+    }
+
+    IEnumerator UpdateCage()
+    {
+        while(true)
+        {
+
+
+            yield return null;
+        }
+    }
+}
