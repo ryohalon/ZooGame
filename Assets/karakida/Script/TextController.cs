61cf3f024e3c71790d55cf3b5334deb2d052c2a4
PS15A07\vantan <karakida.0417@gmail.com>
2016-11-07 17:52:59 +0900

    会話シーンのメッセージ送り

---
+++ Assets/karakida/Script/TextController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour
+{
+    public string[] scenarios; // シナリオを格納
+    [SerializeField]
+    Text uiText;        // uiTextへの参照
+
+    [SerializeField]
+    [Range(0.001f, 0.3f)]
+    float intervalForCharacterDisplay = 0.05f;	// 1文字の表示にかかる時間
+    
+
+    private string currentText = string.Empty;  // 現在の文字列
+    private float timeUntilDisplay = 0;     // 表示にかかる時間
+    private float timeElapsed = 1;          // 文字列の表示を開始した時間
+    public int currentLine = 0;             // 現在の行番号
+    private int lastUpdateCharacter = -1;       // 表示中の文字数
+
+
+    // 文字の表示が完了しているかどうか
+    public bool IsCompleteDisplayText
+    {
+        get { return Time.time > timeElapsed + timeUntilDisplay; }
+    }
+
+
+
+    void Start()
+    {
+        SetNextLine();   
+    }
+
+
+    void Update()
+    {
+
+
+        // 文字の表示が完了してるならクリック時に次の行を表示する
+        if (IsCompleteDisplayText)
+        {
+            if (currentLine < scenarios.Length && Input.GetMouseButtonDown(0))
+            {
+                SetNextLine();
+            }
+        }
+        else
+        {
+            // 完了してないなら文字をすべて表示する
+            if (Input.GetMouseButtonDown(0))
+            {
+                timeUntilDisplay = 0;
+            }
+        }
+
+        int displayCharacterCount = (int)(Mathf.Clamp01((Time.time - timeElapsed) / timeUntilDisplay) * currentText.Length);
+        if (displayCharacterCount != lastUpdateCharacter)
+        {
+            uiText.text = currentText.Substring(0, displayCharacterCount);
+            lastUpdateCharacter = displayCharacterCount;
+        }
+
+
+    }
+
+    
+
+    void SetNextLine()
+    {
+        currentText = scenarios[currentLine];
+        timeUntilDisplay = currentText.Length * intervalForCharacterDisplay;
+        timeElapsed = Time.time;
+        currentLine++;
+        lastUpdateCharacter = -1;
+    }
+
+}
