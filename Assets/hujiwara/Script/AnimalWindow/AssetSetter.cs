34da37cbfce969a674bdc87c8ee9f62497afc298
totopizza <gren6158@yahoo.co.jp>
2016-11-10 17:06:47 +0900

    ショップ画面更新

---
+++ Assets/hujiwara/Script/AnimalWindow/AssetSetter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AssetSetter : MonoBehaviour
+{
+    public FoodStatusAsset _asset;
+
+    public GameObject _text;
+
+    public void SetAsset()
+    {
+        var test = _text.GetComponent<TextChanger>();
+        test._food = _asset;
+
+        test.UpdateFoodStatusAsset();
+    }
+}
---
+++ Assets/hujiwara/Script/BuyWindow/AnimalBuyer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimalBuyer : MonoBehaviour
+{
+    AnimalStatusManager animalStatus;
+
+    public void Sell()
+    {
+        if(!animalStatus.IsPurchase)
+        {
+            animalStatus.IsPurchase = true;
+        }
+    }
+}
