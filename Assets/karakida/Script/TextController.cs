cf19534e8c497f8aac1f638b6ebdc0036d9f5f9a
PS15A07\vantan <karakida.0417@gmail.com>
2016-11-21 12:28:45 +0900

    とりあえず現在の状況をあげておきます

--- Assets/karakida/Script/TextController.cs
+++ Assets/karakida/Script/TextController.cs
@@ -1,24 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+class Scenario
+{
+    public string Name { get; private set; }
+
+    public string Message { get; private set; }
+
+    public Scenario(string name, string message)
+    {
+        Name = name;
+        Message = message;
+    }
+
+}
 
 public class TextController : MonoBehaviour
 {
-    public string[] scenarios; // シナリオを格納
+    private string[] scenarios;   // シナリオを格納する
+
     [SerializeField]
-    Text uiText;        // uiTextへの参照
+    Text uiText;                  // uiTextへの参照を保つ
+    
 
     [SerializeField]
     [Range(0.001f, 0.3f)]
-    float intervalForCharacterDisplay = 0.05f;	// 1文字の表示にかかる時間
-    
+    float intervalForCharacterDisplay = 0.05f;   // 1文字の表示にかかる時間
 
-    private string currentText = string.Empty;  // 現在の文字列
-    private float timeUntilDisplay = 0;     // 表示にかかる時間
-    private float timeElapsed = 1;          // 文字列の表示を開始した時間
-    public int currentLine = 0;             // 現在の行番号
-    private int lastUpdateCharacter = -1;       // 表示中の文字数
-
+    private string currentText = string.Empty;   // 現在の文字列
+    private float timeUntilDisplay = 0;          // 表示にかかる時間
+    private float timeElapsed = 1;               // 文字列の表示を開始した時間
+    private int currentLine = 0;                 // 現在の行番号
+    private int lastUpdateCharacter = -1;        // 表示中の文字数
 
     // 文字の表示が完了しているかどうか
     public bool IsCompleteDisplayText
@@ -26,54 +43,103 @@ public class TextController : MonoBehaviour
         get { return Time.time > timeElapsed + timeUntilDisplay; }
     }
 
-
-
     void Start()
     {
-        SetNextLine();   
+        StartCoroutine(ScenarioIterator());
+        //SetNextLine();
     }
 
-
-    void Update()
+    private IEnumerator ScenarioIterator()
     {
-
-
-        // 文字の表示が完了してるならクリック時に次の行を表示する
-        if (IsCompleteDisplayText)
+        var scenario = LoadScenario();
+        foreach (var line in scenario)
         {
-            if (currentLine < scenarios.Length && Input.GetMouseButtonDown(0))
+            //nameText.text = line.Name;
+            yield return StartCoroutine(LineIterator(line.Message));             // 一文字ずつの表示
+            while (!Input.GetMouseButtonDown(0)) { yield return null; }  // 文字を次の行に行かせるためのクリック待ち
+        }
+
+        SceneManager.LoadScene("Test");     // scenarioが最後の行まで行ったら移動先のシーンに移動
+    }
+
+    private IEnumerator LineIterator(string line)
+    {
+        for (int i = 0; i < line.Length; i++)
+        {
+            var startTime = Time.time;
+            var t = 0F;
+            uiText.text = line.Substring(0, i);
+            
+
+            yield return null;
+
+            while (t < intervalForCharacterDisplay)
             {
-                SetNextLine();
+                t += Time.time - startTime;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    uiText.text = line;
+                    
+                    yield break;
+                }
+                yield return null;
             }
         }
-        else
-        {
-            // 完了してないなら文字をすべて表示する
-            if (Input.GetMouseButtonDown(0))
-            {
-                timeUntilDisplay = 0;
-            }
-        }
-
-        int displayCharacterCount = (int)(Mathf.Clamp01((Time.time - timeElapsed) / timeUntilDisplay) * currentText.Length);
-        if (displayCharacterCount != lastUpdateCharacter)
-        {
-            uiText.text = currentText.Substring(0, displayCharacterCount);
-            lastUpdateCharacter = displayCharacterCount;
-        }
-
-
     }
 
-    
-
-    void SetNextLine()
+    private IEnumerable<Scenario> LoadScenario()
     {
-        currentText = scenarios[currentLine];
-        timeUntilDisplay = currentText.Length * intervalForCharacterDisplay;
-        timeElapsed = Time.time;
-        currentLine++;
-        lastUpdateCharacter = -1;
+        var ta = Resources.Load<TextAsset>("test");
+        foreach(var line in ta.text.Split('\n'))
+        {
+            var s = line.Split(',');
+            yield return new Scenario(s[0], s[1]);
+        }
     }
 
+    //void Update()
+    //{
+    //    // 文字の表示が完了してるならクリック時に次の行を表示する
+    //    if (IsCompleteDisplayText)
+    //    {
+    //        if (currentLine < scenarios.Length && Input.GetMouseButtonDown(0))
+    //        {
+    //            SetNextLine();
+    //        }
+    //    }
+    //    else
+    //    {
+    //        // 完了してないなら文字をすべて表示する
+    //        if (Input.GetMouseButtonDown(0))
+    //        {
+    //            timeUntilDisplay = 0;
+    //        }
+    //    }
+
+    //    // クリックから経過した時間が想定表示時間の何%か確認し、表示文字数を出す
+    //    int displayCharacterCount = (int)(Mathf.Clamp01((Time.time - timeElapsed) / timeUntilDisplay) * currentText.Length);
+
+    //    // 表示文字数が前回の表示文字数と異なるならテキストを更新する
+    //    if (displayCharacterCount != lastUpdateCharacter)
+    //    {
+    //        uiText.text = currentText.Substring(0, displayCharacterCount);
+    //        lastUpdateCharacter = displayCharacterCount;
+    //    }
+    //}
+
+
+    //void SetNextLine()
+    //{
+
+    //    currentText = scenarios[currentLine];
+
+    //    // 想定表示時間と現在の時刻をキャッシュ
+    //    timeUntilDisplay = currentText.Length * intervalForCharacterDisplay;
+    //    timeElapsed = Time.time;
+    //    currentLine++;
+
+    //    // 文字カウントを初期化
+    //    lastUpdateCharacter = -1;
+    //}
 }
