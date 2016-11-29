ffae1d4455b747f9c3d3b1daacbca6d20f5557cd
PS15A13\vantan_2 <dry.tm.fe@icloud.com>
2016-11-29 13:02:06 +0900

    Push

---
+++ Assets/Nomoto/Scripts/MoveInko.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveInko : MonoBehaviour
+{
+
+    float moveSpeed;
+
+    float moveTime;
+
+
+    RectTransform rectTransform = null;
+
+    [SerializeField]
+    float moveChangeTime = 1.0f;
+
+    void Start()
+    {
+        rectTransform = this.gameObject.GetComponent<RectTransform>();
+        moveSpeed = 0.05f;
+        moveTime = 0.0f;
+    }
+
+    void Update()
+    {
+        moveTime += Time.deltaTime;
+        var Rotate = this.GetComponent<RectTransform>().rotation;
+        Rotate.z += Time.deltaTime * moveSpeed;
+        this.GetComponent<RectTransform>().rotation = Rotate;
+
+        if (moveTime > moveChangeTime)
+        {
+            moveTime = 0.0f;
+            moveSpeed *= -1;
+        }
+    }
+}
