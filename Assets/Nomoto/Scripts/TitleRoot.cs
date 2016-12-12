57ee2a955e28134618599d7d63d84b2b52e24ac2
PS15A13\vantan_2 <dry.tm.fe@icloud.com>
2016-12-12 17:09:07 +0900

    UP

---
+++ Assets/Nomoto/Scripts/TitleRoot.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleRoot : MonoBehaviour
+{
+
+    void Start()
+    {
+        Sound.PlayBgm("GameMainBgm");
+    }
+
+    public void PushButton()
+    {
+        Sound.StopBgm();
+        Sound.PlaySe("Ok");
+    }
+
+}
