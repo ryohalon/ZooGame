a53018f04995b1abb7ad75594358932b902f045a
ryouchida <soccer.ry05@gmail.com>
2016-12-25 04:22:16 +0900

    fadeinの変更 peopleMover, peoplemanagerの追加

---
+++ Assets/uchida/Script/People/PeopleManager.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class PeopleManager : MonoBehaviour
+{
+    private AnimalStatusCSV animalStatusCSV = null;
+    [SerializeField]
+    private GameObject people = null;
+
+    private List<GameObject> peopleList = new List<GameObject>();
+    private int maxPeopleNums = 0;
+
+    private float intervalTime = 0.0f;
+    private float maxIntervalTime = 5.0f;
+
+    private float[] peopleSpawnPoint = new float[]
+    {
+        0.0f, 0.0f,
+        0.0f, 0.0f,
+        0.0f, 0.0f
+    };
+
+    void Start()
+    {
+        animalStatusCSV = GameObject.Find("AnimalList").GetComponent<AnimalStatusCSV>();
+
+        StartCoroutine(UpdatePeopleManager());
+    }
+
+    private IEnumerator UpdatePeopleManager()
+    {
+        while(true)
+        {
+            ChangePeopleNums();
+
+            SpawnPeople();
+
+            UpdatePeople();
+
+            yield return null;
+        }
+    }
+
+    private void ChangePeopleNums()
+    {
+        intervalTime += Time.deltaTime;
+        if (intervalTime < maxIntervalTime)
+            return;
+
+        intervalTime = 0.0f;
+
+        float visitors = 0.0f;
+
+        var animals = animalStatusCSV.animals;
+        foreach(var animal in animals)
+        {
+            var animalStatus = animal.GetComponent<AnimalStatusManager>();
+            if (animalStatus.status.CageID == -1)
+                continue;
+
+            visitors += animalStatus.GetVisitors(1.0f);
+        }
+
+        maxPeopleNums = (int)visitors;
+    }
+
+    private void SpawnPeople()
+    {
+        if (peopleList.Count >= maxPeopleNums)
+            return;
+
+        for(int i = 0; i <= (maxPeopleNums - peopleList.Count); i++)
+        {
+            
+        }
+    }
+
+    private void UpdatePeople()
+    {
+        for(int i = 0; i < peopleList.Count; i++)
+        {
+            var people = peopleList[i].GetComponent<PeopleMover>();
+            if (people.isMoveEnd)
+                Destroy(peopleList[i]);
+        }
+    }
+}
---
+++ Assets/uchida/Script/People/PeopleMover.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class PeopleMover : MonoBehaviour
+{
+    public bool isMoveEnd = false;
+
+    float moveTime = 0.0f;
+    public List<float> moveTakeTimeList = new List<float>();
+    public List<Vector2> movePointList = new List<Vector2>();
+    int moveCount = 0;
+    Easing easing = null;
+    public Vector2 start_pos = Vector2.zero;
+
+    void Start()
+    {
+        easing = GetComponent<Easing>();
+
+        StartCoroutine(UpdatePeopleMover());
+    }
+
+    private IEnumerator UpdatePeopleMover()
+    {
+       while(true)
+        {
+            Moving();
+
+            yield return null;
+        }
+    }
+
+    private void Moving()
+    {
+        if (isMoveEnd)
+            return;
+
+        moveTime += Time.deltaTime;
+        float pos_x = easing.Linear(easing.DelayTime(moveTime, 0.0f, moveTakeTimeList[moveCount]), start_pos.x, movePointList[moveCount].x);
+        float pos_y = easing.Linear(easing.DelayTime(moveTime, 0.0f, moveTakeTimeList[moveCount]), start_pos.y, movePointList[moveCount].y);
+
+        transform.localPosition = new Vector3(pos_x, pos_y, 0.0f);
+        if(moveTime >= 1.0f)
+        {
+            start_pos = movePointList[moveCount];
+            moveCount++;
+            if (moveCount >= movePointList.Count)
+                isMoveEnd = true;
+        }
+    }
+}
